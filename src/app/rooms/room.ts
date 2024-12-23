baae84aa61126e3ade8ad66084bcefb9e45b8989
latecomer04 <65734859+latecomer04@users.noreply.github.com>
2024-12-23 23:43:30 +0530

    Directives , pipes and component communication (@Input()) (#1)
    * ngIf directive
    
    * boot-strap changes
    
    * @Input() changes

--- src/app/rooms/room.ts
+++ src/app/rooms/room.ts
@@ -1,5 +1,17 @@
 export interface Room {
   totalRooms: number;
-  availableRooms: number;
+  availableRooms?: number;
   bookedRooms: number;
+}
+
+
+export interface RoomList{
+  roomNumber:number;
+  roomType:string;
+  amenities:string;
+  price:number;
+  photos:string;
+  checkinTime:Date;
+  checkoutTime:Date;
+  rating: number;
 }
